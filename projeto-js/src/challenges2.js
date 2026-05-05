12c094d81ead1d6b639c9fbdde2d06f3e94d37d9
Gustavo Prates Mendes de Aguiar <gustavopma90@gmail.com>
2026-05-04 21:39:27 -0300

    xablau

---
+++ projeto-js/src/challenges2.js
@@ -0,0 +1,20 @@
+// Desafio 11 - Thales
+function generatePhoneNumber() {
+  // seu código aqui
+}
+
+// Desafio 12 - Thales
+function triangleCheck() {
+  // seu código aqui
+}
+
+// Desafio 13 - Thales
+function hydrate() {
+  // seu código aqui
+}
+
+module.exports = {
+  generatePhoneNumber,
+  hydrate,
+  triangleCheck,
+};
