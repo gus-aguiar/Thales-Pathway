5c1282f7b45ad647f8f042ed9a61ecd0c5d39852
Thales <thalesalv@hotmail.com>
2026-05-15 18:24:57 -0300

    desafios 6 a 8 completos

--- projeto-js/src/challenges.js
+++ projeto-js/src/challenges.js
@@ -40,21 +40,67 @@ function footballPoints(vitoria, empate) {
 
 }
 
-
 // Desafio 6 - Thales
-function highestCount() {
-  // seu código aqui
+function highestCount(count) {
+  
+  let index;
+  let maiorNum = count[0];
+  let contagem = 0;
+
+  for(index = 0; index < count.length; index += 1){
+    if(count[index] > maiorNum){
+
+      maiorNum = count[index];
+    }
+  }
+
+  for(index = 0; index < count.length; index += 1){
+    if(count[index] === maiorNum)
+      contagem += 1;
+  }
+  return contagem;
 }
 
 // Desafio 7 - Thales
-function catAndMouse() {
-  // seu código aqui
+function catAndMouse(rato, cat1, cat2) {
+  
+  let catA1 = Math.abs(cat1 - rato);
+  let catA2 = Math.abs(cat2 - rato);
+
+  if(catA1 > catA2){
+    return 'cat2';
+  }
+  else if(catA2 > catA1){
+    return 'cat1';
+  }
+  else if(catA1 === catA2){
+    return 'os gatos trombam e o rato foge';''
+  }
 }
 
 // Desafio 8 - Thales
-function fizzBuzz() {
-  // seu código aqui
+function fizzBuzz(fB) {
+
+  let resposta = [];
+
+  for(index = 0; index < fB.length; index += 1){
+
+  if(fB[index] % 3 === 0 && fB[index] % 5 === 0){
+    resposta.push("fizzBuzz");
+  }
+  else if(fB[index] % 3 === 0){
+    resposta.push("fizz");
+  }
+  else if(fB[index] % 5 === 0){
+    resposta.push("buzz");
+  }
+  else {
+    resposta.push("bug!");
+  }
+ }
+ return resposta;
 }
+console.log(fizzBuzz([2, 15, 7, 9, 45]));
 
 // Desafio 9 - Thales
 function encode() {
