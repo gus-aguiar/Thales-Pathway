3446b805e106bc7e5f47dfe405e7c1b05e618261
Thales <thalesalv@hotmail.com>
2026-05-22 18:53:36 -0300

    desafio 11 completo

--- projeto-js/src/challenges2.js
+++ projeto-js/src/challenges2.js
@@ -1,7 +1,55 @@
 // Desafio 11 - Thales
-function generatePhoneNumber() {
-  // seu código aqui
-}
+function generatePhoneNumber(telefone) {
+
+  let quant = {};
+
+  for(let index = 0; index < telefone.length; index += 1){
+
+    let numero = telefone[index];
+
+    if(quant[numero]){
+      quant[numero] += 1;
+    }
+    else{
+      quant[numero] = 1;
+    }
+  }
+  //console.log(quant);
+
+  let repeticao = [];
+
+  repeticao = (Object.values(quant));
+
+  let maior = Math.max(...repeticao);
+
+  //console.log(maior);
+
+  if(telefone.length != 11){
+    return "Array com tamanho incorreto.";
+  }
+  else if(Math.max(maior) > 2){
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+  else{
+  for(let index = 0; index < telefone.length; index += 1){
+    if(telefone[index] < 0){
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+    else if(telefone[index] > 9){
+      return "não é possível gerar um número de telefone com esses valores";
+    }
+    }
+    }
+
+    let numTel = "(" + telefone[0] + telefone[1] + ") " +telefone[2] + telefone[3] +
+    telefone[4] + telefone[5] + telefone[6] +
+    "-" + telefone[7] + telefone[8] + telefone[9] + telefone[10];
+
+    //console.log(numTel);
+    
+    return numTel;
+  }
+generatePhoneNumber([3, 1, 9, 8, 3, 3, 5, 2, 2, 9, 3]);
 
 // Desafio 12 - Thales
 function triangleCheck() {
